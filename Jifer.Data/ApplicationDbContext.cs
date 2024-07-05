62882ffc194d4b66101c1bc31b5ae07e71ce674d
alex <rlgalexbgto@gmail.com>
2024-07-05 23:54:00 +0300

    Added data layer

---
+++ Jifer.Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace Jifer.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity;
+
+    /// <summary>
+    /// Database Application Context
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        /// <summary>
+        /// Database Application Context Constructor
+        /// </summary>
+        /// <param name="options">Options for the DbContext</param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        
+    }
+}
